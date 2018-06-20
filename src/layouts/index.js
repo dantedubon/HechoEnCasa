7dc4022ca5c0c27142847ab02590e0a7649fed3f
dante.dubon <dante.dubon@gmail.com>
2018-06-19 21:10:57 -0600

    First commit

---
+++ src/layouts/index.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import netlifyIdentity from 'netlify-identity-widget';
+import { Grid } from 'material-ui';
+import ApolloClient from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { withClientState } from 'apollo-link-state';
+import { ApolloProvider } from 'react-apollo';
+import 'medium-draft/lib/index.css';
+// Import Google Fonts
+import 'typeface-merriweather';
+import 'typeface-open-sans';
+// Global styles
+import './index.css';
+// Relative imports
+import withRoot from '../utils/withRoot';
+import logoImage from '../images/logo.png';
+import styled from '../utils/styled';
+import Menu from '../components/Menu';
+import Footer from '../components/Footer';
+import { loginUser, logoutUser } from '../utils/identityActions';
+import Link from 'gatsby-link';
+// Create Apollo Client
+const httpLink = createHttpLink({
+  uri: process.env.GATSBY_GRAPHQLCMS_ENDPOINT,
+});
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const user = netlifyIdentity.currentUser();
+  const token = process.env.GATSBY_GRAPHQLCMS_TOKEN;
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: user ? `Bearer ${token}` : '',
+    },
+  };
+});
+const defaults = {
+  nodes: [],
+};
+const cache = new InMemoryCache();
+const stateLink = withClientState({ cache, defaults });
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+});
+
+const LayoutWrapper = styled(Grid, {
+  component: 'div',
+  container: true,
+  spacing: 24,
+  direction: 'row',
+  alignItems: 'flex-start',
+  justify: 'flex-start',
+})(theme => ({
+  background: theme.palette.background.default,
+}));
+
+const LayoutContent = styled(Grid)(theme => ({
+  textAlign: 'center',
+}));
+const Logo = styled('img')(theme => ({
+  marginTop: theme.spacing.unit * 4,
+  height: 196,
+}));
+const Main = styled('main')(theme => ({
+  backgroundColor: 'white',
+  borderBottom: `1px solid ${theme.palette.grey[200]}`,
+  borderTop: `1px solid ${theme.palette.grey[200]}`,
+  margin: `0 -${theme.spacing.unit * 2}px`,
+  padding: `0 ${theme.spacing.unit * 2}px`,
+}));
+const Wrapper = styled('div')(theme => ({
+  padding: `0 ${theme.spacing.unit * 2}px`,
+}));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize NetlifyIdentity
+    typeof window !== 'undefined' && netlifyIdentity.init();
+  }
+  render() {
+    const { children, data: { site } } = this.props;
+    return (
+      <ApolloProvider client={client}>
+        <Wrapper>
+          <Helmet
+            title={site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: site.siteMetadata.description },
+            ]}
+          />
+          <Link to="/">
+            <Logo src={logoImage} alt={site.siteMetadata.title} />
+          </Link>
+          <LayoutWrapper>
+            <LayoutContent item xs={1}>
+              <Menu data={{ site }} />
+            </LayoutContent>
+            <LayoutContent item xs={11}>
+              <Main>{children()}</Main>
+            </LayoutContent>
+          </LayoutWrapper>
+
+          <Footer data={{ site }} />
+        </Wrapper>
+      </ApolloProvider>
+    );
+  }
+}
+App.propTypes = {
+  children: PropTypes.func,
+};
+export default withRoot(App);
+
+export const query = graphql`
+  query GetSiteMetadata {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
